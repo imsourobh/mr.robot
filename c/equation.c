d45fad0a14a887cd043b59a28eba8f7c799a8ec2
imsourobh <100500795+imsourobh@users.noreply.github.com>
2022-07-18 07:56:32 +0600

    equation2 from book

--- c/equation.c
+++ c/equation.c
@@ -1,17 +1,30 @@
 // simple algerbric equation
 
+
 #include<math.h>
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    float x1,x2;
+    int flag=0, count=0; 
+    float guess, root, test, error;
 
-    printf("Enter the value of a,b,c:");
-    scanf("%d%d%d",&a,&b,&c);
-    x1= (-b+(float)pow((b*b)-(4*a*c),0.5))/(2*a);
-    x2= (-b-(float)pow((b*b)-(4*a*c),0.5))/(2*a);
+    printf("Initial Guess:");
+    scanf("%f",&guess);
 
-    printf("Value of X1 and X2 is respectively:%d %d",x1,x2);
+
+    while (flag)
+    {
+        count++;
+        if (count<50)
+        {
+            test=(10-pow(guess,5));
+            if (test>0)
+            {
+                root=pow(test,(1/2));
+            }
+        }
+    }
+    
+
 
 }
