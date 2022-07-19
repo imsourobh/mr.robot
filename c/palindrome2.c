46eb490c70e3d4f8760f9bcb1736615cf968e892
imsourobh <100500795+imsourobh@users.noreply.github.com>
2022-07-19 20:15:23 +0600

    palindrome from book

---
+++ c/palindrome2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+int main()
+{
+    char name[20];
+    int count, countback, loop=1;
+
+    while(loop==1)
+    {
+        printf("\n enter ");
+        scanf("%s",&name);
+        
+        for (count=0, countback=strlen(name)-1; count<=(strlen(name)-1); count++, countback--)
+        {
+            if (name[count]==' ' || name[count]=='.' || name[count]==',' || name[count]=='/')
+            continue;
+
+            else if (toupper(name[count])!=toupper(name[countback])){
+                printf("Not palindrome");
+                loop=2;
+                break;
+            }
+        }
+        if (count==strlen(name))
+        {loop=2;
+
+    
+        printf("Palindrome");
+        }
+    }
+}
