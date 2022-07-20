272016cbe2622a188519d9e804cfaa86f712e3af
imsourobh <100500795+imsourobh@users.noreply.github.com>
2022-07-20 08:25:12 +0600

    hello

--- c/test.c
+++ c/test.c
@@ -2,13 +2,6 @@
 
 int main()
 {
-    char lower(char c1);
-    {
-        char c2;
-        c2=(c1>='a' && c1<='z') ? ('A'+c1 -'a') :c1;
-        return(c2);
-    }
 
-    char n[10]="hello";
-    printf("%s",lower(n));
+    printf("hello");
 }
