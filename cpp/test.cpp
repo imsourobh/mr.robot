e83d00158db171dfa8dd2ab6ee61918921a0b964
imsourobh <100500795+imsourobh@users.noreply.github.com>
2022-07-26 07:07:47 +0600

    init

---
+++ cpp/test.cpp
@@ -0,0 +1,11 @@
+// first cpp program
+
+#include<iostream> // no need to write .h as the header file
+
+using namespace std;
+
+int main()
+{
+    cout<< "C++ is better than C.\n"; // this is c++ statement
+    return 0;
+}
